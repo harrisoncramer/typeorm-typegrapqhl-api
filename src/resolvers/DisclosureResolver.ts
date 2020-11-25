c5b846e031696a22e52bcee84f4dd467c356dd1c
Harrison Cramer <harrisoncramer@gmail.com>
2020-11-25 12:43:25 -0500

    added where clause to skip and limit search for disclosure entity

--- src/resolvers/DisclosureResolver.ts
+++ src/resolvers/DisclosureResolver.ts
@@ -21,7 +21,7 @@ class SkipLimitInput {
   @Field()
   orderField: string;
   @Field({ nullable: true })
-  order: "ASC" | "DSC";
+  order?: "ASC" | "DSC";
   @Field()
   skip: number;
   @Field()
@@ -31,25 +31,24 @@ class SkipLimitInput {
 @InputType()
 class FindDisclosuresInput extends SkipLimitInput {
   @Field({ nullable: true })
-  field: string;
+  field?: string;
 
   @Field({ nullable: true })
-  filter: string;
+  filter?: string;
 }
 
 @Resolver()
 export class DisclosureResolver {
   @Query(() => [Disclosure])
   async findDisclosures(@Arg("input") input: FindDisclosuresInput) {
-    let results = await getRepository(Disclosure)
-      .createQueryBuilder()
-      .where(`disclosure.${input.field} like :name`, {
+    let query = getRepository(Disclosure).createQueryBuilder();
+    // If input field and input filter provided, add WHERE clause to query
+    if (input.field && input.filter) {
+      query.where(`disclosure.${input.field} like :name`, {
         name: `%${input.filter}%`,
-      })
-      .skip(input.skip)
-      .limit(input.limit)
-      .getMany();
-
+      });
+    }
+    let results = await query.skip(input.skip).limit(input.limit).getMany();
     return results;
   }
 
