7f9b3fd19607517d484034282fa8103c87c5db61
Harrison Cramer <harrisoncramer@gmail.com>
2020-11-25 21:56:44 -0500

    reverted to non-serverless

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,36 @@
 import "reflect-metadata";
+import path from "path";
 import dotenv from "dotenv";
 import { createConnection, getConnectionOptions } from "typeorm";
 import express from "express";
-import { ApolloServer as ApolloServerDev } from "apollo-server-express";
-import { ApolloServer as ApolloServerProd } from "apollo-server-lambda";
+import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
 import { DisclosureResolver } from "./resolvers/DisclosureResolver";
 
-import { APIGatewayProxyEvent, Context, Callback } from "aws-lambda";
-
+// Set environment variables
 const environment = process.env.NODE_ENV;
+dotenv.config({ path: path.resolve(__dirname, `.env.${environment}`) });
 
-// Connect to database, regardless of in development or production
-const connectToDBAndBuildSchema = async () => {
-  const options = await getConnectionOptions(environment || "development");
-  const connection = await createConnection({ ...options, name: "default" });
-  if (!connection.isConnected) {
-    throw new Error("Could not connect to database!");
-  }
-  const schema = await buildSchema({
-    resolvers: [HelloWorldResolver, DisclosureResolver],
-    validate: true,
+(async () => {
+  const app = express();
+
+  const options = await getConnectionOptions(
+    process.env.NODE_ENV || "development"
+  );
+  await createConnection({ ...options, name: "default" });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver, DisclosureResolver],
+      validate: true,
+    }),
+    context: ({ req, res }) => ({ req, res }),
   });
-  return { connection, schema };
-};
 
-// This function will only be run in production
-// and exposes a handler function for our aws-lambda
-// context to hit.
-export const graphql = async (
-  event: APIGatewayProxyEvent,
-  context: Context,
-  callback: Callback
-) => {
-  const { schema } = await connectToDBAndBuildSchema();
-  const server = new ApolloServerProd({ schema });
-  let handler = server.createHandler({
-    cors: { origin: "*", credentials: false },
+  apolloServer.applyMiddleware({ app, cors: false });
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}/graphql`);
   });
-  return handler(event, context, callback);
-};
-
-// If in development mode, run the application
-// using apollo-server-express and ts-node locally
-// for fast reload and quick DB connections
-if (environment === "development") {
-  (async () => {
-    dotenv.config({ path: ".env.development" });
-    const { schema } = await connectToDBAndBuildSchema();
-    const app = express();
-    const server = new ApolloServerDev({
-      schema,
-      context: ({ req, res }) => ({ req, res }),
-    });
-    server.applyMiddleware({ app, cors: false });
-    app.listen(process.env.PORT, () => console.log("SERVER LISTENING..."));
-  })();
-}
+})();
