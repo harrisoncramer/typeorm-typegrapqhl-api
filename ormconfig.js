42ccd800d30f4843c914f9c3084f07a83236e320
Harrison Cramer <harrisoncramer@gmail.com>
2021-02-15 15:27:47 -0500

    functioning in dev

--- ormconfig.js
+++ ormconfig.js
@@ -11,11 +11,16 @@ module.exports = [
     password: "postgres",
     username: "postgres",
     database: "postgres",
-    synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
-    logging: false,
-    entities: ["./dist/entity/*.js"],
-    migrations: ["./dist/migration/*.js"],
-    subscribers: ["./dist/subscriber/*.js"],
+    synchronize: true,
+    logging: true,
+    entities: ["src/entity/**/*.ts"],
+    migrations: ["src/migration/**/*.ts"],
+    subscribers: ["src/subscriber/**/*.ts"],
+    cli: {
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migration",
+      subscribersDir: "src/subscriber",
+    },
   },
   {
     name: "production",
