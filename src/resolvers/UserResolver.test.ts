2e93d21e90059efabb4f737f2617cb0524f18af9
Harrison Cramer <harrisoncramer@gmail.com>
2021-02-28 15:03:16 -0500

    added install and uninstall scripts to install into all running containers, but only when development container is online

--- src/resolvers/UserResolver.test.ts
+++ src/resolvers/UserResolver.test.ts
@@ -1,6 +1,7 @@
 import { connect } from "../postgres";
 import { Connection } from "typeorm";
 import { gCall } from "../tests/gCall";
+import faker from "faker";
 
 let conn: Connection;
 beforeAll(async () => {
@@ -16,25 +17,70 @@ mutation Register($input: UserInput!) {
   register(
     input: $input
   ) {
-    id
+    name
     email
   }
 }
 `;
 
 describe("User resolver", () => {
-  it("Should succeed", async () => {
+  const user = {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+
+  const userInvalidEmail = {
+    name: faker.name.firstName(),
+    email: "notemail",
+    password: faker.internet.password(),
+  };
+
+  const userInvalidPassword = {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: "weak",
+  };
+
+  it("Should register user with valid credentials.", async () => {
     const response = await gCall({
       source: registerMutation,
       variableValues: {
-        input: {
-          name: "bob3",
-          email: "bob3@bob.com",
-          password: "asdfasdf",
-        },
+        input: user,
       },
     });
 
+    expect(response).toMatchObject({
+      data: {
+        register: {
+          name: user.name,
+          email: user.email,
+        },
+      },
+    });
+  });
+
+  it("Should reject user with invalid email.", async () => {
+    const response = await gCall({
+      source: registerMutation,
+      variableValues: {
+        input: userInvalidEmail,
+      },
+    });
+
+    console.log(JSON.stringify(response));
+    // Check that response matches what we'd expect...
+  });
+
+  it("Should reject user with invalid password.", async () => {
+    const response = await gCall({
+      source: registerMutation,
+      variableValues: {
+        input: userInvalidPassword,
+      },
+    });
+
+    console.log(JSON.stringify(response));
     // Check that response matches what we'd expect...
   });
 });
