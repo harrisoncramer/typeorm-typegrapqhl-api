8d97de9aef26995b2b1dbc0c46d36b4a066aabba
Harrison Cramer <harrisoncramer@gmail.com>
2021-02-28 10:03:45 -0500

    added script for easy startup/shutdown

--- src/tests/gCall.ts
+++ src/tests/gCall.ts
@@ -1,4 +1,4 @@
-import { graphql } from "graphql";
+import { graphql, GraphQLSchema } from "graphql";
 import { Maybe } from "graphql/jsutils/Maybe";
 import { createSchema } from "../utils/createSchema";
 
@@ -9,10 +9,16 @@ interface Options {
   }>;
 }
 
-// Allows us to make calls to our Graphql Schema inside our tests more easily
+// Define schema up here so it only needs to be created once, not on every gCall.
+let schema: GraphQLSchema;
+
+// Higher order function that allows us to make calls to our Graphql Schema inside our tests more easily
 export const gCall = async ({ source, variableValues }: Options) => {
+  if (!schema) {
+    schema = await createSchema();
+  }
   return graphql({
-    schema: await createSchema(),
+    schema,
     source,
     variableValues,
   });
